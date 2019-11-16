f7cee6073dbe998d511f0a46d326fc48bd4ed7b3
theramses <41164729+theramses@users.noreply.github.com>
2019-11-16 17:10:19 +0100

    Add files via upload

---
+++ sesion 1/pulsador_con_pullup/pulsador_con_pullup.ino
@@ -0,0 +1,30 @@
+/*
+  pulsador
+
+  enciende y apaga el led conectado al pin digital 2
+  cuando se presiona el pulsador conectado al pin digital 7.
+
+  el circuito se muestra en la imagen pulsador con pullup.
+
+*/
+
+const int pulsador = 7;     // the number of the pushbutton pin
+const int led = 2;      // the number of the LED pin
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (digitalRead(buttonPin) == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
